52002b0b94196937b4a3fe9d1cff564fca61ce1b
alice <alice@example.com>
2021-02-23 22:01:42 +0000

    up

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -L -o vendor/gcc-10.2.0.tar.xz http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-10.2.0/gcc-10.2.0.tar.xz
+curl -L -o vendor/base.txz http://ftp1.jp.freebsd.org/pub/FreeBSD/releases/amd64/12.2-RELEASE/base.txz
+curl -L -o vendor/binutils-2.36.1.tar.xz https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz
+
